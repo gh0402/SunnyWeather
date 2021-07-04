499c004e8f7b884a8c296aef2f1336566842aad6
gg0402 <1114061971@qq.com>
2021-07-04 15:20:13 +0800

    加入显示天气信息功能

--- app/src/main/java/com/example/sunnyweather/MainActivity.kt
+++ app/src/main/java/com/example/sunnyweather/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.sunnyweather
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //让背景和系统状态栏融合
+        val decorView = window.decorView
+        decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.statusBarColor = Color.TRANSPARENT
     }
 }
--- app/src/main/java/com/example/sunnyweather/logic/model/PlaceResponse.kt
+++ app/src/main/java/com/example/sunnyweather/logic/model/PlaceResponse.kt
@@ -2,9 +2,9 @@ package com.example.sunnyweather.logic.model
 
 import com.google.gson.annotations.SerializedName
 
-data class PlaceResponse(val status: String, val places: List<Place>)
+class PlaceResponse(val status: String, val places: List<Place>)
 
-data class Place(val name: String, val location: Location,
+class Place(val name: String, val location: Location,
                  @SerializedName("formatted_address") val address: String)
 
-data class Location(val lng: String, val lat: String)
+class Location(val lng: String, val lat: String)
